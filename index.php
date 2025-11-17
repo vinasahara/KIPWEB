56aec176cceafba755d7ee60a0075df3ef3b0e1a
vinasahara <fnashara8@gmail.com>
2025-11-17 15:22:24 +0700

    Skema 1: edit baris yang sama - versi Vina

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home KIP-K</title>
+  <title>Home KIP-Kuliah</title>
 
   <!-- Tailwind CSS -->
   <script src="https://cdn.tailwindcss.com"></script>
