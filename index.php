58dd7067e9fa3d273979b9d64f56ead1a0da37c4
CindyNaysillaIsmail <cindynsllismail@gmail.com>
2025-11-17 15:22:08 +0700

    Skema 1: edit baris yang sama - versi Cindy

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Beranda | Sistem Informasi KIP-Kuliah POLINELA</title>
+  <title>Beranda KIP-KULIAH</title>
 
   <!-- Tailwind CSS -->
   <script src="https://cdn.tailwindcss.com"></script>
